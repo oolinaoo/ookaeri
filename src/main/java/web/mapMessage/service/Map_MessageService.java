82b002cd33008e3bcbb158a9366da0c5185a6116
Newgate <newgate789456123@gmail.com>
2021-12-23 20:22:02 +0800

    no message

--- src/main/java/web/mapMessage/service/Map_MessageService.java
+++ src/main/java/web/mapMessage/service/Map_MessageService.java
@@ -33,6 +33,22 @@ public class Map_MessageService {
 		return map_messagevo;
 		
 	}
+	public Map_MessageVO updateMap_MessageVO(Integer MAP_MSG_NO,String MAP_STORE_NO, 
+			String MEM_ACCT,String MAP_MSG_CONTENT,Timestamp MAP_MSG_TIME,Integer MAP_MSG_STATE)
+	{
+		Map_MessageVO map_messagevo = new Map_MessageVO();
+		map_messagevo.setMAP_MSG_CONTENT(MAP_MSG_CONTENT);
+		map_messagevo.setMAP_MSG_NO(MAP_MSG_NO);
+		map_messagevo.setMAP_MSG_STATE(MAP_MSG_STATE);
+		map_messagevo.setMAP_MSG_TIME(MAP_MSG_TIME);
+		map_messagevo.setMAP_STORE_NO(MAP_STORE_NO);
+		map_messagevo.setMEM_ACCT(MEM_ACCT);
+		
+		dao.update(map_messagevo);
+		
+		return map_messagevo;
+		
+	}
 	
 	public List<Map_MessageVO> getAll() {
 		return dao.getAll();
--- src/main/java/web/mapReport/service/Map_ReportService.java
+++ src/main/java/web/mapReport/service/Map_ReportService.java
@@ -28,6 +28,20 @@ public class Map_ReportService {
 		dao.add(map_rept);
 		return map_rept;
 	}
+	public Map_ReportVO updateMap_ReportDAO(Integer MAP_REPT_NO, String MAP_REPT_CONTENT, Integer MAP_MSG_NO, String MEM_ACCT,
+			Integer MAP_REPT_STATE, String ADMIN_ACCT)
+	{
+		Map_ReportVO map_rept = new Map_ReportVO();
+		map_rept.setADMIN_ACCT(ADMIN_ACCT);
+		map_rept.setMAP_MSG_NO(MAP_MSG_NO);
+		map_rept.setMAP_REPT_CONTENT(MAP_REPT_CONTENT);
+		map_rept.setMAP_REPT_NO(MAP_REPT_NO);
+		map_rept.setMAP_REPT_STATE(MAP_REPT_STATE);
+		map_rept.setMEM_ACCT(MEM_ACCT);
+		
+		dao.update(map_rept);
+		return map_rept;
+	}
 	
 	
 	public List<Map_ReportVO> getAll() {
